eda87a3ba3bd43315875a166198c22b9584a6f0b
agomezneo <agomez@proyectoneo.com>
2022-09-26 10:44:23 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,7 @@ function MyApp({ Component, pageProps }) {
             clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
           },
           exitState: {
+            opacity: 0,
             clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
           },
         }}
