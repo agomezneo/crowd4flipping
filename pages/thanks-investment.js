29628e7ff5c8548098272f54cb4ee1df9d9786c1
agomezneo <agomez@proyectoneo.com>
2022-10-10 12:47:27 +0200

    thanks updated

--- pages/thanks-investment.js
+++ pages/thanks-investment.js
@@ -1,79 +1,57 @@
 import styles from '../styles/Home.module.scss';
 import Layout from "../components/layouts/Layout";
-import { motion, useScroll } from "framer-motion"
+import { motion } from "framer-motion"
 import Link from "next/link";
-import { BsFillTreeFill } from "react-icons/bs";
 import Image from 'next/image';
 import Logo from '../public/images/brand/crowd4flipping/PNG_Files/Transparent_Logo_02.png'
-import Header from '../components/headers/header_pages';
 
 export default function InvesmentThanksPage() {
-
+  
   return (
-    <>
       <Layout>
-        <Hero />
+          <div className={styles.explore_page_hero}>
+            <div className={styles.page_grid_container}>
+              <motion.div
+                initial={{ y: 25, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  delay: 0.2,
+                  duration: 0.75,
+                }}
+                className={`${styles.explore_page_hero_container} ${styles.left}`}
+              >
+                <Link href='/' className={styles.logo_container}>
+                  <Image src={Logo} width={700} height={300} alt='Crowd for Flipping' />
+                  <div className={styles.text_container}>
+                      <h1>La inversión inmobiliaria <span>al alcance de todos</span></h1>
+                  </div>
+                </Link>
+                <div className={styles.thanks_message_container}>
+                  <h2>Tú solicitud ha sido recibida con éxito.<span>Uno de nuestros agentes se pondrá en contacto contigo en breve.</span></h2>
+                </div>
+              </motion.div>   
+              <motion.div
+                initial={{ y: 25, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  delay: 0.2,
+                  duration: 0.75,
+                }}
+                className={`${styles.explore_page_hero_container} ${styles.right}`}
+              >
+              </motion.div>   
+            </div>
+            <motion.div
+              initial={{ y: 25, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                delay: 0.4,
+                duration: 0.75,
+              }}
+              className={styles.mountains}
+            >
+          </motion.div>
+        </div>
       </Layout>
-
-    </>
   );
 }
-
-const Hero = () => {
-  return (
-    <div className={styles.explore_page_hero}>
-      <div className={styles.page_grid_container}>
-        <motion.div
-          initial={{ y: 25, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            delay: 0.2,
-            duration: 0.75,
-          }}
-          className={`${styles.explore_page_hero_container} ${styles.left}`}
-        >
-          <Link href='/' className={styles.logo_container}>
-            <Image src={Logo} width={700} height={300} alt='Crowd for Flipping' />
-            <div className={styles.text_container}>
-                <h1>La inversión inmobiliaria <span>al alcance de todos</span></h1>
-            </div>
-          </Link>
-          <div className={styles.thanks_message_container}>
-            <h2>Tú solicitud ha sido recibida con éxito.<span>Uno de nuestros agentes se pondrá en contacto contigo en breve.</span></h2>
-            
-            {/* <Link href="/inversor">
-              <a className={styles.back_btn}>Quiero invertir</a>
-            </Link>
-            <Link href="/propietario">
-              <a className={styles.back_btn}>Ofrece tu propiedad</a>
-            </Link> */}
-          </div>
-        </motion.div>   
-        <motion.div
-          initial={{ y: 25, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            delay: 0.2,
-            duration: 0.75,
-          }}
-          className={`${styles.explore_page_hero_container} ${styles.right}`}
-        >
-        </motion.div>   
-      </div>
-      <Mountains />
-    </div>
-  );
-};
-
-const Mountains = () => (
-  <motion.div
-    initial={{ y: 25, opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{
-      delay: 0.4,
-      duration: 0.75,
-    }}
-    className={styles.mountains}
-  >
-  </motion.div>
-);
