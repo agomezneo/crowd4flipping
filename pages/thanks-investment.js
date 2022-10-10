b357cd0756b4d29e1f1c832f3a527fa75ea3d9ff
agomezneo <agomez@proyectoneo.com>
2022-10-10 11:28:21 +0200

    Update thanks-investment.js

--- pages/thanks-investment.js
+++ pages/thanks-investment.js
@@ -6,7 +6,8 @@ import { BsFillTreeFill } from "react-icons/bs";
 import Image from 'next/image';
 import Logo from '../public/images/brand/crowd4flipping/PNG_Files/Transparent_Logo_02.png'
 import Header from '../components/headers/header_pages';
-export default function Home() {
+
+export default function InvesmentThanksPage() {
 
   return (
     <>
