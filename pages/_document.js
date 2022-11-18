3ca280629d5e48c936d590aa742d0ba7234f27e4
agomezneo <agomez@proyectoneo.com>
2022-11-18 17:00:22 +0100

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <meta name="facebook-domain-verification" content="s1jdwvgkd52qz97y64bh9fpls3zgn8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <div id="modal-root"></div>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
