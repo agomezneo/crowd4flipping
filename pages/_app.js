3373a7c8671485b2d09ff3acb908c874ab7b682d
agomezneo <agomez@proyectoneo.com>
2023-01-31 11:13:57 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,29 @@ import styles from '../styles/globals.scss'
 import  { motion, AnimatePresence } from 'framer-motion';
 import {useRouter} from 'next/router';
 import Script from 'next/script';
+import {pageview, FB_PIXEL_ID} from '../../lib/fpixel'
+
+const handleRouteChange = () => {
+  pageview()
+}
+
+const pageview = () => {
+  window.fbq('track', 'PageView')
+}
 
 function MyApp({ Component, pageProps }) {
 
 
   const router = useRouter();
+
+  useEffect(() => {
+    // the below will only fire on route changes (not initial load - that is handled in the script below)
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
     <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
@@ -22,6 +40,23 @@ function MyApp({ Component, pageProps }) {
             });
         `}
     </Script>
+
+
+    <Script id="facebook-pixel">
+      {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '609162034305973');
+        fbq('track', 'PageView');
+      `}
+    </Script>
+
     <AnimatePresence exitBeforeEnter>
       <motion.div
         className={styles.back_ground_traslate}
